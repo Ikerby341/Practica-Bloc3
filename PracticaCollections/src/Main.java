82571908d555dff021c1279b1826551b86940d88
Ikerby <novoliva@gmail.com>
2025-03-10 08:35:56 +0100

    Ara podem afegir els 3 tipus de productes

--- PracticaCollections/src/Main.java
+++ PracticaCollections/src/Main.java
@@ -1,4 +1,9 @@
+import Productes.Productes;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
+import Productes.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,13 +26,43 @@ public class Main {
                         case 0:
                             break;
                         case 1:
-
+                            Vista.msg("Afegir alimentació");
+                            Vista.msg("Nom producte:");
+                            String n = scan.next();
+                            Vista.msg("Preu: ");
+                            Float p = scan.nextFloat();
+                            Vista.msg("Data caducitat: ");
+                            LocalDate d = LocalDate.parse(scan.next());
+                            Vista.msg("Codi de barres: ");
+                            int c = scan.nextInt();
+                            Alimentacio aliment = new Alimentacio(n,p,c,d);
+                            Model.afegirAlCarret(aliment);
                             break;
                         case 2:
-
+                            Vista.msg("Afegir tèxtil");
+                            Vista.msg("Nom producte:");
+                            String n1 = scan.next();
+                            Vista.msg("Preu: ");
+                            Float p1 = scan.nextFloat();
+                            Vista.msg("Composició tèxtil: ");
+                            String ct1 = scan.next();
+                            Vista.msg("Codi de barres: ");
+                            int c1 = scan.nextInt();
+                            Textil aliment1 = new Textil(n1,p1,c1,ct1);
+                            Model.afegirAlCarret(aliment1);
                             break;
                         case 3:
-
+                            Vista.msg("Afegir tèxtil");
+                            Vista.msg("Nom producte:");
+                            String n2 = scan.next();
+                            Vista.msg("Preu: ");
+                            Float p2 = scan.nextFloat();
+                            Vista.msg("Garantia (dies): ");
+                            String g2 = scan.next();
+                            Vista.msg("Codi de barres: ");
+                            int c2 = scan.nextInt();
+                            Textil aliment2 = new Textil(n2,p2,c2,g2);
+                            Model.afegirAlCarret(aliment2);
                             break;
                     }
             }
--- PracticaCollections/src/Model.java
+++ PracticaCollections/src/Model.java
@@ -1,2 +1,11 @@
+import Productes.Productes;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Model {
+    private static Queue<Productes> carret = new ArrayDeque<>();
+    protected static void afegirAlCarret(Productes p){
+        carret.add(p);
+    }
 }
